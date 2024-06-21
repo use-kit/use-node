9d8e631760a6051ae041ec4c76f3f595d7796fd7
akashi <akashi_sai@163.com>
2024-06-21 13:35:42 +0800

    :art: feat: copy

--- src/node/copy/index.ts
+++ src/node/copy/index.ts
@@ -1,11 +1,12 @@
 // import fs from 'node:fs'
+import fastGlob from 'fast-glob'
 import fsExtra from 'fs-extra'
 
-export function copy(from: string, to: string) {
+export function copy(from: string, to: string, filter?: string) {
   fsExtra.copy(from, to, { filter: (file) => {
     // console.log(file)
     const isFile = fsExtra.statSync(file).isFile()
-    const isTsFile = isFile && file.endsWith('.ts')
+    const isTsFile = isFile && file.endsWith(filter ?? '.ts')
     const isExampleDir = !isFile
     return isTsFile || isExampleDir
     // return true
@@ -13,3 +14,31 @@ export function copy(from: string, to: string) {
 }
 
 // copy('src/node/copy', 'src/c', '.ts')
+
+export const copyDirectory = async (source: string, destination: string, filterFn: (file: string) => boolean) => {
+  const files = await fastGlob(`${source}/**/*`, {
+    onlyFiles: false,
+    deep: Infinity,
+  })
+
+  for (const file of files) {
+    if (filterFn(file)) {
+      const relativePath = file.replace(source, '')
+      const newPath = destination + relativePath
+      if (fsExtra.statSync(file).isFile()) {
+        fsExtra.copyFileSync(file, newPath)
+      }
+      else {
+        fsExtra.ensureDirSync(newPath)
+      }
+    }
+  }
+}
+
+export const filterFunction = (file: string) => {
+  const isFile = fsExtra.statSync(file).isFile()
+  const isTxtFile = isFile && file.endsWith('.txt')
+  const isExampleDir = !isFile && file.includes('/example')
+
+  return isTxtFile || isExampleDir
+}
